411a1189259eb846d280ce3f8153f4374f7c32ac
Richie Preece <rwpreece@xactware.com>
2014-10-06 14:07:16 -0600

    Bugfix for windows-specific issues

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ server.listen(app.get('port'), function (err) {
   if (err) { console.error(err); process.exit(-1); }
 
   // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
+  if (process.getuid && process.getuid() === 0) {
     require('fs').stat(__filename, function(err, stats) {
       if (err) { return console.error(err); }
       process.setuid(stats.uid);
